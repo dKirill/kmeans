7d7a0f56a464adfaff085f8cedce79a5d081da02
dKirill <1starfall1@gmail.com>
2018-01-13 16:17:13 +0300

    initial: cmake and base structure

---
+++ kmeans.cpp
@@ -0,0 +1,8 @@
+//
+//  kmeans.cpp
+//  kmeans
+//
+//  Created by Кирилл Делимбетов on 13.01.2018.
+//
+
+#include "kmeans.hpp"
---
+++ kmeans.hpp
@@ -0,0 +1,13 @@
+//
+//  kmeans.hpp
+//  kmeans
+//
+//  Created by Кирилл Делимбетов on 13.01.2018.
+//
+
+#ifndef kmeans_hpp
+#define kmeans_hpp
+
+#include <stdio.h>
+
+#endif /* kmeans_hpp */
