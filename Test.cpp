2b5b9f4a1c682ea67a3c2c06e5b18c5fab732a3c
dKirill <1starfall1@gmail.com>
2018-01-14 21:29:39 +0300

    add bench

--- Test.cpp
+++ Test.cpp
@@ -9,6 +9,7 @@
 #define CATCH_CONFIG_MAIN
 
 #include <algorithm>
+#include <chrono>
 #include <numeric>
 #include <random>
 #include <catch.hpp>
@@ -18,9 +19,10 @@ using namespace clustering;
 
 // MARK: Settings
 struct Constant {
-	static constexpr auto dimNum = 10;
+	static constexpr auto dimNum = 20;
 	static constexpr auto batchSize = 10000;
 	static constexpr auto centerCount = 10;
+	static constexpr auto benchRuns = 10;
 };
 
 template<class FloatType>
@@ -29,29 +31,29 @@ static bool fequal(const FloatType& f1, const FloatType& f2) {
 }
 
 TEST_CASE("L1 norm") {
-	using VSpace = VectorSpace<Constant::dimNum>;
-	VSpace::Element elem1;
-	VSpace::Element elem2;
+	using VSpace10 = VectorSpace<10>;
+	VSpace10::Element elem1;
+	VSpace10::Element elem2;
 	
 	// fill elements
 	std::iota(elem1.begin(), elem1.end(), 0);
 	std::iota(elem2.rbegin(), elem2.rend(), 0);
 	
-	const auto manhattanDistance = VSpace::l1norm(elem1, elem2);
+	const auto manhattanDistance = VSpace10::l1norm(elem1, elem2);
 	
 	REQUIRE(fequal(manhattanDistance, 50.f));
 }
 
 TEST_CASE("L2 norm") {
-	using VSpace = VectorSpace<Constant::dimNum>;
-	VSpace::Element elem1;
-	VSpace::Element elem2;
+	using VSpace10 = VectorSpace<10>;
+	VSpace10::Element elem1;
+	VSpace10::Element elem2;
 	
 	// fill elements
 	std::iota(elem1.begin(), elem1.end(), 0);
 	std::iota(elem2.rbegin(), elem2.rend(), 0);
 	
-	const auto euclideanDistance = VSpace::l2norm(elem1, elem2);
+	const auto euclideanDistance = VSpace10::l2norm(elem1, elem2);
 	
 	REQUIRE(fequal(euclideanDistance, 18.165902f));
 }
@@ -98,6 +100,7 @@ void kmeansTest() {
 		}
 	}
 }
+
 TEST_CASE("kmeans l1") {
 	using VSpace = VectorSpace<Constant::dimNum>;
 	kmeansTest<VSpace::l1norm>();
@@ -106,4 +109,46 @@ TEST_CASE("kmeans l1") {
 TEST_CASE("kmeans l2") {
 	using VSpace = VectorSpace<Constant::dimNum>;
 	kmeansTest<VSpace::l2norm>();
+}
+
+
+template<VectorSpace<Constant::dimNum>::Norm norm>
+void kmeansBench() {
+	using VSpace = VectorSpace<Constant::dimNum>;
+	VSpace::ElementsBatch elements(Constant::batchSize);
+	VSpace::TerminationCriteria termCriteria = { 0.1f, 10000 };
+	std::vector<VSpace::Element> centers(Constant::centerCount);
+	std::vector<VSpace::ElementsBatch::size_type> elementToClusterMap(elements.size());
+	// init random generator
+	std::default_random_engine rengine;
+	std::uniform_real_distribution<VSpace::BaseType> distribution(0.0,1.0);
+	VSpace::RandomGenerator generator = [&rengine, &distribution] { return distribution(rengine); };
+	
+	// generate random data
+	for(auto& elem : elements) {
+		std::generate(elem.begin(), elem.end(), [&generator] {
+			return 1000 * generator();
+		});
+	}
+	
+	// measure
+	auto t1 = std::chrono::high_resolution_clock::now();
+	
+	for(int i = 0; i < Constant::benchRuns; ++i) {
+		// do clustering
+		REQUIRE(VSpace::kmeans<norm>(elements, termCriteria, generator, centers, elementToClusterMap));
+	}
+	
+	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>( std::chrono::high_resolution_clock::now() - t1 ).count();
+	OUT(__PRETTY_FUNCTION__ << " takes " << (duration / Constant::benchRuns)  << "ms per run\n");
+}
+
+TEST_CASE("bench l1") {
+	using VSpace = VectorSpace<Constant::dimNum>;
+	kmeansBench<VSpace::l1norm>();
+}
+
+TEST_CASE("bench l2") {
+	using VSpace = VectorSpace<Constant::dimNum>;
+	kmeansBench<VSpace::l2norm>();
 }
